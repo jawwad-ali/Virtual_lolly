3aefc3ff2a0d8142f4f6c36029f4b925e3d3013c
jawwad-ali <alijawwad001@gmail.com>
2020-12-02 17:53:27 +0500

    error fixed

--- gatsby-node.js
+++ gatsby-node.js
@@ -37,40 +37,40 @@
 
 
 
-const path = require("path")
-exports.createPages = async ({ graphql, actions }) => {
-  const { createPage } = actions
+// const path = require("path")
+// exports.createPages = async ({ graphql, actions }) => {
+//   const { createPage } = actions
 
-  const { data } = await graphql(`
-       {
-        get_v_Card {
-            getVCard { 
-              c1
-               c2
-               c3
-               rec
-               msg
-               sender
-               url
-            }
-          }
-        }
-  `)
+//   const { data } = await graphql(`
+//        {
+//         get_v_Card {
+//             getVCard { 
+//               c1
+//                c2
+//                c3
+//                rec
+//                msg
+//                sender
+//                url
+//             }
+//           }
+//         }
+//   `)
 
-  data.get_v_Card.getVCard.forEach(node => {
-    createPage({
-      path: `lolly/${node.url}`,
-      component: path.resolve("./src/templates/template.tsx"),
-      context: {
-        id: node.url,
-        c1: node.c1,
-        c2: node.c2,
-        c3: node.c3,
-        url: node.url,
-        msg: node.msg,
-        sender: node.sender,
-        rec: node.rec
-      },
-    })
-  })
-}
+//   data.get_v_Card.getVCard.forEach(node => {
+//     createPage({
+//       path: `lolly/${node.url}`,
+//       component: path.resolve("./src/templates/template.tsx"),
+//       context: {
+//         id: node.url,
+//         c1: node.c1,
+//         c2: node.c2,
+//         c3: node.c3,
+//         url: node.url,
+//         msg: node.msg,
+//         sender: node.sender,
+//         rec: node.rec
+//       },
+//     })
+//   })
+// }
